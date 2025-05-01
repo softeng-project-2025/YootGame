ac1921fbc2cf19e0e4287eca19cbba48bdeb0b43
Jimin Lim <jimin3060@naver.com>
2025-05-01 22:26:21 +0900

    [feat] Board 클래스: 전략 기반 말 이동 처리

---
+++ src/model/board/Board.java
@@ -0,0 +1,33 @@
+package model;
+
+import model.piece.Piece;
+import model.position.Position;
+import model.yut.YutResult;
+import model.strategy.PathStrategy;
+
+public class Board {
+
+    private PathStrategy pathStrategy;
+
+    public Board(PathStrategy pathStrategy) {
+        this.pathStrategy = pathStrategy;
+    }
+
+    // Piece가 현재 위치에서 YutResult에 따라 다음 위치로 이동
+    public Position getNextPosition(Position current, YutResult result) {
+        return pathStrategy.getNextPosition(current, result);
+    }
+
+    public PathStrategy getPathStrategy() {
+        return pathStrategy;
+    }
+
+    public void setPathStrategy(PathStrategy pathStrategy) {
+        this.pathStrategy = pathStrategy;
+    }
+
+    public void movePiece(Piece piece, YutResult result) {
+        Position next = getNextPosition(piece.getPosition(), result);
+        piece.setPosition(next);
+    }
+}
