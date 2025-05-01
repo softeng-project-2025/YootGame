44feee75d527e9ba14c3885cd7f69377cc3981e6
Jimin Lim <jimin3060@naver.com>
2025-05-01 22:28:23 +0900

    [feat] Player 클래스: 이름, 말 리스트, 완료 여부 체크 구현

---
+++ src/model/player/Player.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.piece.Piece;
+
+public class Player {
+    private String name;
+    private List<Piece> pieces;
+
+    public Player(String name, int pieceCount) {
+        this.name = name;
+        this.pieces = new ArrayList<>();
+        for (int i = 0; i < pieceCount; i++) {
+            pieces.add(new Piece(this, i));
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+
+    // 특정 말을 선택했을 때 해당 말을 반환 (id로 찾기)
+    public Piece getPieceById(int id) {
+        return pieces.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    // 모든 말을 완주했는지 확인 (승리 조건)
+    public boolean hasFinishedAllPieces() {
+        return pieces.stream().allMatch(Piece::isFinished);
+    }
+
+    // 움직일 수 있는 말이 존재하는지
+    public boolean hasMovablePieces() {
+        return pieces.stream().anyMatch(p -> !p.isFinished());
+    }
+}
