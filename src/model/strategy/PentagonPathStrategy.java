c2730f104e1b22a8443b5f0e18f071a0d1fdb087
Jimin Lim <jimin3060@naver.com>
2025-05-02 19:40:32 +0900

    [chore] PentagonPathStrategy 클래스 틀 생성 (향후 오각형 경로 확장용)

---
+++ src/model/strategy/PentagonPathStrategy.java
@@ -0,0 +1,19 @@
+package model.strategy;
+
+import model.position.Position;
+import model.yut.YutResult;
+
+public class PentagonPathStrategy implements PathStrategy {
+
+    @Override
+    public Position getNextPosition(Position current, YutResult result) {
+        // TODO: 중심점 판단 및 1번 path vs 2번 path 규칙 구현
+        throw new UnsupportedOperationException("PentagonPathStrategy is not implemented yet.");
+    }
+
+    @Override
+    public java.util.List<Position> getPath() {
+        // TODO: 오각형 경로 규칙 구현
+        throw new UnsupportedOperationException("PentagonPathStrategy path not defined.");
+    }
+}
