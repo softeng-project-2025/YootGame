88104e57284a140973cd4079e41bb3be664d9431
Jimin Lim <jimin3060@naver.com>
2025-05-02 19:40:36 +0900

    [refactor] YutThrower: 랜덤 윷 결과 생성 로직 정리 및 리팩토링

--- src/model/yut/YutThrower.java
+++ src/model/yut/YutThrower.java
@@ -1,4 +1,32 @@
 package model.yut;
 
+import java.util.Random;
+
 public class YutThrower {
+
+    private static final Random random = new Random();
+
+    public static YutResult throwYut() {
+        int count = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int stick = random.nextInt(2); // 0 or 1
+            count += stick;
+        }
+
+        switch (count) {
+            case 0:
+                return YutResult.MO;
+            case 1:
+                return YutResult.DO;
+            case 2:
+                return YutResult.GAE;
+            case 3:
+                return YutResult.GEOL;
+            case 4:
+                return YutResult.YUT;
+            default:
+                return YutResult.DO; // fallback
+        }
+    }
 }
