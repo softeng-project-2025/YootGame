403f2343713b9a06f9ae09b345c771acafa15fcf
Kredsya <do901328@gmail.com>
2025-05-12 19:24:16 +0900

    [refactPath] remove unused library

--- src/main/java/model/strategy/SquarePathStrategy.java
+++ src/main/java/model/strategy/SquarePathStrategy.java
@@ -5,9 +5,7 @@ import model.position.Position;
 import model.yut.YutResult;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import model.piece.PathType;
 
 /**
